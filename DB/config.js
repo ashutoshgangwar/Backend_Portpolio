c795fecaa66c37de4be1ed098b0b10374d36129e
Ashutosh Gangwar <ashutoshgangwar1994@gmail.com>
2024-11-12 21:34:54 +0530

    Mongodb atlas URL added

--- DB/config.js
+++ DB/config.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // Replace '1234' with your actual password and remove the angle brackets
-mongoose.connect('mongodb://localhost:27017/registation', {
+mongoose.connect('mongodb+srv://ashutoshgangwar1994:3tYg3mz6arUuv1MQ@my-portpholio.vbzlr.mongodb.net/', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => {
