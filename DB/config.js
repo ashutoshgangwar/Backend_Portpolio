76f75f9f1f205e58a59fed4d6225209e3bac5da8
Ashutosh Gangwar <ashutoshgangwar1994@gmail.com>
2024-11-13 20:46:18 +0530

    some changes

--- DB/config.js
+++ DB/config.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-<<<<<<< HEAD
+
 // Replace '1234' with your actual password and remove the angle brackets
 mongoose.connect('mongodb+srv://ashutoshgangwar1994:3tYg3mz6arUuv1MQ@my-portpholio.vbzlr.mongodb.net/registation', {
   useNewUrlParser: true,
@@ -10,12 +10,5 @@ mongoose.connect('mongodb+srv://ashutoshgangwar1994:3tYg3mz6arUuv1MQ@my-portphol
 }).catch(err => {
   console.error('Connection error', err);
 });
-=======
-mongoose.connect('mongodb+srv://ashutoshgangwar1994:3tYg3mz6arUuv1MQ@my-portpholio.vbzlr.mongodb.net/registation')
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch(err => {
-    console.error('Connection error', err);
-  });
->>>>>>> 50477339d26e847cacf74ca16a4b2b08d877ae4c
+
+
